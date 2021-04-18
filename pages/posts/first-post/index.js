65fc69c04ef88c963d4f9db4aa763becff4f170c
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2021-04-18 09:11:36 +0300

    just fin the metadata part

--- pages/posts/first-post/index.js
+++ pages/posts/first-post/index.js
@@ -1,8 +1,13 @@
 ﻿import Link from 'next/link'
+import Head from "next/head";
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>First Post</title>
+      </Head>
+
       <h1>First Post</h1>
       <h2>
         <Link href="/">
