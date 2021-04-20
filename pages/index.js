5012aea35502047a5e1016fcfd1d914941a1c627
Alexey Zakharov <alexey.zakharov@sarov-itc.ru>
2021-04-20 10:45:16 +0300

    added dynamic routes

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,9 @@ import Head from 'next/head'
 
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
+  const allPostsData = []
 
   return {
     props: {
